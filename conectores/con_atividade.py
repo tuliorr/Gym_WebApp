0ddfe5b1bb9c2a65412ba3b43681a2460caab011
tuliorr <tulio.rodrigues.r@hotmail.com>
2023-06-02 10:46:40 -0300

    update conectores

--- conectores/con_atividade.py
+++ conectores/con_atividade.py
@@ -35,38 +35,126 @@ def get_all():
     
     return atividades
 
-# Função para obter as informações de uma atividade
-def get_one(id):
+# Função para obter alunos de uma atividade
+def get_alunos(id):
     
-    sql = 'SELECT * FROM webuser.ATIVIDADES WHERE id = %s'
+    alunos = []
     
+    sql = 'SELECT alunos.* FROM webuser.ALUNOS INNER JOIN webuser.AGENDAMENTOS ON alunos.id = webuser.AGENDAMENTOS.aluno \
+        WHERE webuser.AGENDAMENTOS.atividade = %s'
+        
     value = [id]
+    
+    results = runSQL(sql, value)
+    
+    for row in results:
+        
+        aluno = Aluno(row["nome"],
+                      row["sobrenome"],
+                      row["data_nascimento"],
+                      row["endereco"],
+                      row["telefone"],
+                      row["email"],
+                      row["tipo_plano"],
+                      row["data_inicio"],
+                      row["ativo"],
+                      row["id"])
+        
+        alunos.append(aluno)
+        
+    return alunos
+    
+
+# Função para obter atividades ativas a partir de uma data
+def active_on_date(data):
+    
+    sql = 'SELECT * FROM webuser.ATIVIDADES WHERE ativo = true ORDER BY nome ASC where data = %s'
+    
+    value = [data]
     
     result = runSQL(sql, value)[0]
     
+    atividades = []
+    
     if result is not None:
+        
+        for row in result:
+            
+            tipo_plano = plano.get_one(row['plano'])
+            
+            atividade = Atividade(row['nome'],
+                                  row['instrutor'],
+                                  row['data'],
+                                  row['duracao'],
+                                  row['capacidade'],
+                                  tipo_plano,
+                                  row['ativo'],
+                                  row['id'])
+            
+            atividades.append(atividade)
+            
+    return atividades
+
+# Função para obter as atividades ativas ordenadas por data
+def get_all_active():
+    
+    atividades = []
+    
+    sql = 'SELECT * FROM webuser.ATIVIDADES WHERE ativo = true ORDER BY data ASC'
+    
+    result = runSQL(sql)
+       
+    for row in result:
         
         tipo_plano = plano.get_one(row['plano'])
         
-        atividade = Atividade(result['nome'],
-                              result['instrutor'],
-                              result['data'],
-                              result['duracao'],
-                              result['capacidade'],
+        atividade = Atividade(row['nome'],
+                              row['instrutor'],
+                              row['data'],
+                              row['duracao'],
+                              row['capacidade'],
                               tipo_plano,
-                              result['ativo'],
-                              result['id'])
+                              row['ativo'],
+                              row['id'])
+        
+        atividades.append(atividade)
             
-    return atividade
+    return atividades
 
-# Função para criar um atividade
+# Função para obter as atividades inativas ordenadas por data
+def get_all_inactive():
+    
+    atividades = []
+    
+    sql = 'SELECT * FROM webuser.ATIVIDADES WHERE ativo = false ORDER BY data ASC'
+    
+    result = runSQL(sql)
+       
+    for row in result:
+        
+        tipo_plano = plano.get_one(row['plano'])
+        
+        atividade = Atividade(row['nome'],
+                              row['instrutor'],
+                              row['data'],
+                              row['duracao'],
+                              row['capacidade'],
+                              tipo_plano,
+                              row['ativo'],
+                              row['id'])
+        
+        atividades.append(atividade)
+            
+    return atividades
+
+# Função para criar uma atividade
 def new_one(atividade):
     
     sql = 'INSERT INTO webuser.ATIVIDADES (nome, instrutor, data, duracao, capacidade, tipo_plano, ativo) \
         VALUES (%s, %s, %s, %s, %s, %s, %s) RETURNING *;'
     
-    values = [atividade.nome.id, atividade.instrutor.id, atividade.data.id, atividade.duracao.id,
-              atividade.capacidade.id, atividade.tipo_plano.id, atividade.ativo.id]
+    values = [atividade.nome, atividade.instrutor, atividade.data, atividade.duracao,
+              atividade.capacidade, atividade.tipo_plano, atividade.ativo]
         
     results = runSQL(sql, values)
     
@@ -74,12 +162,32 @@ def new_one(atividade):
         
     return atividade
 
+# Função para deletar uma atividade
+def delete_one(id):
+    
+    sql = 'DELETE FROM webuser.ATIVIDADES WHERE id = %s'
+    
+    values = [id]
+        
+    runSQL(sql, values)
+
+# Função para editar uma atividade
+def edit(atividade):
+    
+    sql = 'UPDATE webuser.ATIVIDADES SET (nome, instrutor, data, duracao, capacidade, tipo_plano, ativo) \
+        VALUES (%s, %s, %s, %s, %s, %s, %s) WHERE atividade = %s'
+        
+    values = [atividade.nome, atividade.instrutor, atividade.data, atividade.duracao,
+              atividade.capacidade, atividade.tipo_plano, atividade.ativo]
+    
+    runSQL(sql, values)
+
 # Função para verificar se uma atividade existe
-def verifica_atividade(nome, instrutor, data):
+def verifica_atividade(atividade, aluno, data):
     
-    sql = 'SELECT * FROM webuser.ATIVIDADES WHERE atividade = %s AND aluno = %s'
+    sql = 'SELECT * FROM webuser.ATIVIDADES WHERE atividade = %s AND aluno = %s AND data = %s'
     
-    value = [atividade, aluno]
+    value = [atividade, aluno, data]
     
     result = runSQL(sql, value)
     
@@ -88,3 +196,28 @@ def verifica_atividade(nome, instrutor, data):
     
     else:
         return True
+    
+# Função para obter uma atividade ativa
+def get_one(id):
+    
+    sql = 'SELECT * FROM webuser.ATIVIDADES WHERE ativo = true AND id = %s'
+    
+    value = [id]
+    
+    result = runSQL(sql, value)[0]
+    
+    if result is not None:
+
+        tipo_plano = plano.get_one(row['plano'])
+        
+        atividade = Atividade(row['nome'],
+                              row['instrutor'],
+                              row['data'],
+                              row['duracao'],
+                              row['capacidade'],
+                              tipo_plano,
+                              row['ativo'],
+                              row['id'])
+        
+    return atividade
+        
---
+++ conectores/con_instrutor.py
@@ -0,0 +1,33 @@
+# Módulo de conexão entre a classe e o banco de dados
+
+# Imports
+
+from classes.instrutor import Instrutor
+from classes.atividade import Atividade
+
+from database.runSQL import runSQL
+
+# Função para listar todos os instrutores
+def get_all():
+    
+    pass
+
+# Função para retornar um instrutor
+def get_one(id):
+    pass
+
+# Função para listar todas as atividades de um instrutor
+def get_activities(instructor_id):
+    pass
+
+# Função para cadastrar um instrutor
+def new_one(instrutor):
+    pass
+
+# Função para deletar um instrutor
+def delete_one(id):
+    pass
+
+# Função para editar um instrutor
+def edit(instrutor):
+    pass
